9506277bed9511619813620a036cb972103d788b
vinhnd <nguyenducvinh09@gmail.com>
2018-12-27 15:18:54 +0700

    creat file index.js , sayHi() method for Dog

--- Dog.js
+++ Dog.js
@@ -1,8 +1,14 @@
-function Dog() {
+const chalk = require('chalk');
+function Dog(name) {
+	this.name = name;
 	this.stomach = [];
 }
 Dog.prototype.eat = function(cat) {
 	// body...
 	this.stomach.push(cat);
+};
+Dog.prototype.sayHi = function() {
+	// body...
+	console.log('Hi, I am a dog , my name is ' +chalk.blue(this.name));
 };
 module.exports = Dog;
